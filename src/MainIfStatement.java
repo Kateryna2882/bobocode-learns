1f45d507e56237264a55260d1b5dd05c698295e6
Kateryna2882 <govoryhina82@gmail.com>
2024-03-13 18:53:48 +0200

    add fields and methods

---
+++ src/MainIfStatement.java
@@ -0,0 +1,17 @@
+public class MainIfStatement {
+    public static void main(String[] args) {
+//        if statement = performs a block of code if it's condition evaluates to be tru
+        int age = 13;
+        if(age >= 75) {
+            System.out.println("You are an adult!");
+        } else if (age >= 18) {
+            System.out.println("Ok Boomer!");
+        } else if (age >= 13) {
+            System.out.println("You are an teenage!");
+
+
+    } else {
+            System.out.println("You are not an adult!");
+        }
+    }
+}
